d2b3d6a06db69e0f699d5cf2997bc5ddc5b34b49
Redha CHENOUF <rchenouf1@myges.fr>
2023-12-01 12:20:37 +0100

    First commit

---
+++ vérité.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Bienvenue sur le Linkedin de Redha CHENOUF\n");
+    printf("Bienvenue sur le Linkedin de Redha CHENOUF\n");
+    printf("Bienvenue sur le Linkedin de Redha CHENOUF\n");
+    
+    return 0;
+}
